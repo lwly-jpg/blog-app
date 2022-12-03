e98ef354b2eba847e605b589d2dc8c8eb06134c3
adonl <adnlwly@gmail.com>
2022-12-03 15:03:52 +0000

    add blogs hook to home component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new blog', body: 'lorem ipsum...', author: 'cli', id: 1 },
+    { title: 'Welcome!', body: 'lorem ipsum...', author: 'deid', id: 2 },
+    { title: 'Another post', body: 'lorem ipsum...', author: 'ari', id: 3 }
+  ])
+
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title } </h2>
+          <p>{ blog.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
