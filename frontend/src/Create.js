172f82f2aa0adbbe4015846dfbc9da17970c1c0f
adonl <adnlwly@gmail.com>
2022-12-03 17:32:16 +0000

    update add post form

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
   return ( 
     <div className="create">
       <h2>Add a new post</h2>
+      <form>
+        <label>Post title:</label>
+        <input 
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Post body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        >
+        </textarea>
+        <label>Author:</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="cli">cli</option>
+          <option value="deid">deid</option>
+        </select>
+        <button>Add post</button>
+      </form>
+      <h2>{ title }</h2>
+      <p>{ body }</p>
+      <p>Auhthor: { author }</p>
     </div>
    );
 }
