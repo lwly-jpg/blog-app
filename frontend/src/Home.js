d490a4a79003d3741e2fbbeb9dd56080fd235779
adonl <adnlwly@gmail.com>
2022-12-03 15:25:18 +0000

    add delete function for posts

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,12 +5,17 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new blog', body: 'lorem ipsum...', author: 'cli', id: 1 },
     { title: 'Welcome!', body: 'lorem ipsum...', author: 'deid', id: 2 },
-    { title: 'Another post', body: 'lorem ipsum...', author: 'ari', id: 3 }
+    { title: 'Another post', body: 'lorem ipsum...', author: 'cli', id: 3 }
   ]);
+
+  const handleDelete = (id) => {
+    const newPosts = blogs.filter(blog => blog.id !== id);
+    setBlogs(newPosts);
+  }
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All posts" />
+      <BlogList blogs={blogs} title="All posts" handleDelete={handleDelete} />
     </div>
    );
 }
