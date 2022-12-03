8a97ab5514e5ae7dd10850d877ba4b06a950fa68
adonl <adnlwly@gmail.com>
2022-12-03 15:16:22 +0000

    move bloglist to separate component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,20 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new blog', body: 'lorem ipsum...', author: 'cli', id: 1 },
     { title: 'Welcome!', body: 'lorem ipsum...', author: 'deid', id: 2 },
     { title: 'Another post', body: 'lorem ipsum...', author: 'ari', id: 3 }
-  ])
+  ]);
 
   return ( 
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title } </h2>
-          <p>{ blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All posts" />
     </div>
    );
 }
