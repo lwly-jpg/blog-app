421b495d5091961985764d896a2a25ce4bfb1746
adonl <adnlwly@gmail.com>
2022-12-03 16:15:00 +0000

    create useFetch hook

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,40 +1,20 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-  const handleDelete = (id) => {
-    const newPosts = blogs.filter(blog => blog.id !== id);
-    setBlogs(newPosts);
-  }
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if (!res.ok) {
-          throw Error('Cannot fetch blog data')
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-  }, []);
+  // const handleDelete = (id) => {
+  //   const newPosts = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newPosts);
+  // }
 
   return ( 
     <div className="home">
       { error && <div>{ error }</div>}
       { isPending && <div>Loading...</div>}
-      <BlogList blogs={blogs} title="All posts" handleDelete={handleDelete} />
+      <BlogList blogs={blogs} title="All posts" />
     </div>
    );
 }
