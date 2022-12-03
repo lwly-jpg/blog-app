171d11753bee53f4ba13bcd92726d2f2412a52cb
adonl <adnlwly@gmail.com>
2022-12-03 16:46:45 +0000

    update nav to use Link tags

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (  
     <nav className="navbar">
       <h1>Bloggy Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{
           color: "white",
           backgroundColor: "#3535f1",
           borderRadius: "8px"
-        }}>New post</a>
+        }}>New post</Link>
       </div>
     </nav>
   );
