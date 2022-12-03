d0ab9f32dd27f1453e4ca683fcf93aab48a7cc60
adonl <adnlwly@gmail.com>
2022-12-03 13:21:03 +0000

    add initial styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './Nav';
 import Home from './Home';
 
--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,7 +4,11 @@ const Navbar = () => {
       <h1>Bloggy Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New post</a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: "#3535f1",
+          borderRadius: "8px"
+        }}>New post</a>
       </div>
     </nav>
   );
