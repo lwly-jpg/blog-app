9a50d47f4ba58dac8a2c0263c9ab6b3b29867d65
adonl <adnlwly@gmail.com>
2022-12-03 13:10:29 +0000

    add nav bar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './Nav';
 
 function App() {
+  const title = "Blog"
+  const likes = 50
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>Blog</h1>
+
       </div>
     </div>
   );
